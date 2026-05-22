8f3d3367fbfcd85594fc4a1773ebf1a3ce59314a
Camille-018 <villemin.camille18@gmail.com>
2026-05-22 10:45:15 +0200

    ajout retour filtre

--- historique/valeurSpecifique.php
+++ historique/valeurSpecifique.php
@@ -107,7 +107,7 @@ $historique = $stmt->fetchAll();
 </head>
 <body>
 <h1>Onduleur - Résultat du Filtre</h1>
-
+<a href="historique.php">⬅ Retour à l’historique</a>
 <!-- affiche les filtres -->
 <?php if (!empty($colonnes)): ?>
 <h3>Filtres appliqués :</h3>
